f9529b0e35345746e505ce4c9688657f810f93c7
lianmatsuo <lianmatsuo146@gmail.com>
2026-04-17 01:31:57 +0100

    feat: add HS3F autoregressive trajectory model
    Hierarchical sparse structured model alongside ForestFlow.

--- packages/models/__init__.py
+++ packages/models/__init__.py
@@ -1,9 +1,11 @@
 """Forest-Flow generative models for time series synthesis."""
 
 from .forest_flow import ForestFlow
+from .hs3f import HS3F
 from .iterator import FlowMatchingDataIterator
 
 __all__ = [
     "ForestFlow",
+    "HS3F",
     "FlowMatchingDataIterator",
 ]
